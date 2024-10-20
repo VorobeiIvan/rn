2cdfabf174b49278046601b691a071b8df3cb4b5
VorobeiIvan <0675361905vi@gmail.com>
2024-10-20 21:12:55 +0300

    add global styles

--- screens/RegistrationScreen/RegistrationScreenStyles.ts
+++ screens/RegistrationScreen/RegistrationScreenStyles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import colors from "../../styles/colors";
+import { colors } from "../../styles/global";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -14,7 +14,6 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
   contentContainer: {
-    height: "66%",
     borderTopRightRadius: 24,
     borderTopLeftRadius: 24,
     backgroundColor: colors.white,
@@ -32,7 +31,89 @@ const styles = StyleSheet.create({
     height: "100%",
     resizeMode: "cover",
   },
-  addAvatarButton: {},
+  addAvatarIcon: {
+    width: 24,
+    height: 24,
+    resizeMode: "cover",
+  },
+  addAvatarButton: {
+    position: "absolute",
+    width: 24,
+    height: 24,
+    bottom: 14,
+    right: -14,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "medium",
+    fontFamily: "Roboto-Medium",
+    textAlign: "center",
+    color: colors.black_primary,
+    marginTop: 32,
+    marginBottom: 32,
+  },
+  inputContainer: {
+    gap: 16,
+    marginHorizontal: 16,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderRadius: 8,
+    borderWidth: 1,
+    color: colors.black_primary,
+    backgroundColor: colors.light_gray,
+    borderColor: colors.border_gray,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    padding: 16,
+  },
+  rightPasswordInputButton: {
+    position: "absolute",
+    right: 0,
+    justifyContent: "center",
+  },
+  rightPasswordInputButtonText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    padding: 16,
+    color: colors.blue,
+  },
+  passwordInputContainer: {
+    position: "relative",
+  },
+  buttonText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    padding: 16,
+    color: colors.white,
+    lineHeight: 20,
+  },
+  primaryButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: colors.orange,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    gap: 16,
+    marginVertical: 44,
+  },
+  secondaryButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 4,
+  },
+  text: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    color: colors.blue,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default styles;
---
+++ styles/global.ts
@@ -0,0 +1,68 @@
+export const colors: { [key: string]: string } = {
+  white: "#FFFFFF",
+  black_primary: "#212121",
+  light_gray: "#F6F6F6",
+  gray: "#9B9B9B",
+  border_gray: "#E8E8E8",
+  blue: "#1B4371",
+  orange: "#FF6C00",
+};
+
+export const fontFamily: { [key: string]: string } = {
+  Roboto: "Roboto",
+};
+
+export const fontWeights: { [key: string]: string } = {
+  regular: "400",
+  medium: "500",
+  bold: "700",
+};
+
+export const fontSizes: { [key: string]: number } = {
+  title: 30,
+  navigation: 17,
+  text: 16,
+  comments: 13,
+  subtitle: 13,
+  lightText: 11,
+  date: 10,
+};
+
+export const letterSpacings: { [key: string]: number } = {
+  title: 0.3,
+  navigation: -0.41,
+};
+
+export const lineHeights: { [key: string]: number } = {
+  date: 12,
+  lightText: 13,
+  subtitle: 15,
+  comments: 18,
+  text: 19,
+  navigation: 22,
+  title: 35,
+};
+
+export const borderRadius: { [key: string]: number } = {
+  comment: 6,
+  input: 8,
+  contentBlock: 8,
+  avatar: 16,
+  container: 24,
+  button: 100,
+};
+
+export const padding: { [key: string]: number } = {
+  input: 16,
+  buttonVertical: 16,
+  buttonHorizontal: 32,
+};
+
+export const margin: { [key: string]: number } = {
+  smallIconText: 4, // Відступи від іконки до тексту
+  mediumIconText: 6, // Відступи від іконки до тексту (інший варіант)
+  regular: 16, // Відступи між елементами в окремому блоці
+  block: 32, // Відступи між блоками
+  buttonBlock: 44, // Відступ від блоку кнопок
+  date: 8, // Відступ до дати
+};
